b29820ec1e26391ef0be36bbf9f146647715b336
Leon Davis <leonskidev@pm.me>
2022-02-16 02:40:27 +0000

    refactor: make the repo nicer

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,6 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   globals: {
-    'ts-jest': {tsconfig: './tsconfig.json'}
-  }
+    'ts-jest': { tsconfig: './tsconfig.json' },
+  },
 };

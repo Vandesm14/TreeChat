a5d116cdd4333e4945274b0faa729724c76076fe
Shane Vandegrift <vandesm14@gmail.com>
2022-03-14 22:16:45 -0400

    complete redo

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,169 @@
-import { render, h } from 'preact';
-import { useState, useEffect } from 'preact/hooks';
+import { render, FunctionalComponent as FC } from 'preact';
+import { useState, StateUpdater, useReducer, useMemo } from 'preact/hooks';
+import { Message, Database, createDefaultMessages } from './shared';
+
+import { Worker } from 'snowflake-uuid';
+const snowflake = new Worker();
+
+function setFromEvent(fn: StateUpdater<string>) {
+  return (e: InputEvent) => {
+    fn((e.target as HTMLInputElement).value);
+  };
+}
+type Action<A> = (action: A) => void;
+
+type MessageProps = {
+  message: Message;
+  setMessages: Action<MessageAction>;
+};
+
+const Message: FC<MessageProps> = ({ message, setMessages }) => {
+  const enterMessage = () => {
+    setMessages({ type: 'enter', payload: message.id });
+  };
+
+  return (
+    <div className="message">
+      <button onClick={enterMessage}>Enter</button>
+      <span>{message.text}</span>
+      <span>
+        {message.children.length ? `(${message.children.length})` : ''}
+      </span>
+    </div>
+  );
+};
+
+type MessageInputProps = {
+  setMessages: Action<MessageAction>;
+};
+
+const MessageInput: FC<MessageInputProps> = ({ setMessages }) => {
+  const [text, setText] = useState('');
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    setMessages({
+      type: 'add',
+      payload: {
+        id: snowflake.nextId(),
+        text: text,
+        children: [],
+        userId: BigInt(0),
+        timestamp: Date.now(),
+      },
+    });
+    setText('');
+  };
+
+  return (
+    <form onSubmit={handleSubmit} class="message-input">
+      <input type="text" value={text} onChange={setFromEvent(setText)} />
+      <button type="submit">Send</button>
+    </form>
+  );
+};
+
+type TopicListProps = {
+  currentTopic: bigint;
+  setCurrentTopic: StateUpdater<bigint>;
+  query: Database;
+};
+
+const TopicList: FC<TopicListProps> = ({
+  currentTopic,
+  setCurrentTopic,
+  query,
+}) => {
+  const topicMsg = query.getMessage(currentTopic);
+  const parent = query.getParentTopic(topicMsg.id);
+
+  const goBack = () => {
+    if (parent) {
+      setCurrentTopic(parent.id);
+    }
+  };
+
+  return (
+    <section className="topics">
+      <p className="topic-current">{topicMsg.topic?.name ?? '(root)'}</p>
+      {parent && (
+        <a href="javascript:void(0)" onClick={goBack}>
+          Go back
+        </a>
+      )}
+    </section>
+  );
+};
+
+type MessageAction =
+  | {
+      type: 'add';
+      payload: Message;
+    }
+  | {
+      type: 'enter';
+      payload: Message['id'];
+    };
 
 const App = () => {
+  const [messages, setMessages] = useReducer(
+    (state: Message[], action: MessageAction) => {
+      let ret: typeof state = [...state];
+      switch (action.type) {
+        case 'add':
+          ret = [...state, action.payload];
+          const topic = messages.find((m) => m.id === currentTopic);
+          if (topic) {
+            topic.children.push(action.payload.id);
+          } else {
+            throw new Error('Topic not found');
+          }
+          break;
+        case 'enter':
+          setCurrentTopic(action.payload);
+          let message = messages.find((m) => m.id === action.payload);
+          if (message) {
+            message.topic = {
+              name: message.text,
+              timestamp: Date.now(),
+            };
+            ret = [...state.filter((m) => m.id !== action.payload), message];
+          } else {
+            throw new Error('Message not found');
+          }
+          break;
+      }
+      localStorage.setItem(
+        'messages',
+        JSON.stringify(ret, (_, v) =>
+          typeof v === 'bigint' ? v.toString() : v
+        )
+      );
+      return ret;
+    },
+    localStorage.getItem('messages')
+      ? JSON.parse(localStorage.getItem('messages') || '[]')
+      : createDefaultMessages()
+  );
+  const [currentTopic, setCurrentTopic] = useState(messages[0].id);
+  const query = useMemo(() => new Database(messages), [messages]);
+
   return (
-    <h1>Hello World!</h1>
+    <main>
+      <TopicList
+        currentTopic={currentTopic}
+        setCurrentTopic={setCurrentTopic}
+        query={query}
+      />
+      <section className="messaging">
+        <div className="message-list">
+          {query.listMessages(currentTopic).map((m) => (
+            <Message message={m} setMessages={setMessages} />
+          ))}
+        </div>
+        <MessageInput setMessages={setMessages} />
+      </section>
+    </main>
   );
 };
 
---
+++ src/shared.ts
@@ -0,0 +1,104 @@
+import { Worker } from 'snowflake-uuid';
+const snowflake = new Worker();
+
+export interface Topic {
+  name: string;
+  timestamp: number;
+}
+
+export interface Message {
+  id: bigint;
+  userId: User['id'];
+  text: string;
+  children?: Message['id'][];
+  topic?: Topic
+  timestamp: number;
+}
+
+export class User {
+  id: bigint;
+  #messages: Message[];
+
+  constructor(id: User['id']) {
+    this.id = id;
+    this.#messages = this.#messages.filter((message) => message.userId === id);
+  }
+
+  messages(): Message[] {
+    return this.#messages;
+  }
+}
+
+export class Database {
+  #messages: Message[];
+
+  constructor(messages: Message[]) {
+    this.#messages = messages;
+  }
+
+  getMessages(): Message[] {
+    return this.#messages;
+  }
+
+  getMessage(id: Message['id']): Message | undefined {
+    return this.#messages.find((message) => message.id === id);
+  }
+
+  listMessages(topicId: Message['id']): Message[] {
+    const topic = this.#messages.find((message) => message.id === topicId);
+    // return the children of the topic
+    const messages = topic.children.map((id) => this.#messages.find((message) => message.id === id));
+    return messages;
+  }
+
+  getUser(userId: User['id']): User | undefined {
+    const user = this.#messages.find((message) => message.userId === userId);
+    return user ? new User(userId) : undefined;
+  }
+
+  getTopic(topicName: string): Message | undefined {
+    return this.#messages.find((message) => message.topic.name === topicName);
+  }
+
+  getTopicById(id: Message['id']): Message | undefined {
+    return this.#messages.find((message) => message.id === id);
+  }
+
+  getParentTopic(id: Message['id']): Message | undefined {
+    const message = this.#messages.find((message) => message.id === id);
+    return message ? this.#messages.find((message) => message.children?.includes(id)) : undefined;
+  }
+
+  getTopics(topicName: string): Message[] {
+    return this.#messages.filter((message) => message.topic.name === topicName);
+  }
+
+  listTopics(limit = 10): Message[] {
+    return this.#messages
+      .filter((message) => message.topic.name)
+      .slice(0, limit);
+  }
+}
+
+export function createDefaultMessages() {
+  // create a list of messages with a default message called "(root)" and with one child called "#general"
+  // both of these messages should be topics with the respective topic names
+
+  const general: Message = {
+    id: snowflake.nextId(),
+    userId: BigInt(1),
+    text: '#general',
+    children: [],
+    timestamp: Date.now(),
+  }
+
+  const root: Message = {
+    id: snowflake.nextId(),
+    userId: BigInt(1),
+    text: '(root)',
+    children: [general.id],
+    timestamp: Date.now(),
+  }
+
+  return [root, general];
+}

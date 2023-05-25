b9f7f1980fc7ba814871107ddb205beff0574df3
Shane Vandegrift <vandesm14@gmail.com>
2023-05-25 14:02:30 -0400

    dynamic protocol string

--- src/gun.ts
+++ src/gun.ts
@@ -1,7 +1,11 @@
 import Gun from 'gun';
 import React from 'react';
 
+const GunURL = `${window.location.protocol}//${window.location.hostname}:8080/gun`;
+
+console.log(`Connecting to ${GunURL}`);
+
 export const gun = Gun({
-  peers: ['http://mr.thedevbird.com:8080/gun'],
+  peers: [GunURL],
 });
 export const gunContext = React.createContext(gun);

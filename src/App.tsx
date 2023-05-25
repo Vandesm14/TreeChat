4a247cffe41e102300876051c53c3ef4b3a6cc11
Shane Vandegrift <vandesm14@gmail.com>
2023-05-25 19:08:05 -0400

    remove back button

--- src/App.tsx
+++ src/App.tsx
@@ -3,23 +3,15 @@ import Chat from './components/Chat';
 import { gun, gunContext } from './gun';
 import { useAtom } from 'jotai';
 import { chatListPathAtom } from './atoms';
-import { Button } from '@blueprintjs/core';
 import Path from './components/Path';
 
 function App() {
-  const [path, setPath] = useAtom(chatListPathAtom);
+  const [path] = useAtom(chatListPathAtom);
 
   return (
     <gunContext.Provider value={gun}>
       <Path path={path} />
       <Chat path={path} />
-      {path.length > 1 ? (
-        <Button
-          onClick={() => setPath((path) => path.slice(0, path.length - 1))}
-        >
-          Back
-        </Button>
-      ) : null}
     </gunContext.Provider>
   );
 }

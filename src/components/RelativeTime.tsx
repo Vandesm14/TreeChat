249f6685e600101d660fd0b7dba3d2214207e427
Shane Vandegrift <vandesm14@gmail.com>
2023-05-25 14:37:23 -0400

    relative time i18n

--- src/components/RelativeTime.tsx
+++ src/components/RelativeTime.tsx
@@ -1,16 +1,32 @@
-import { Popover2, Tooltip2 } from '@blueprintjs/popover2';
+import { Tooltip2 } from '@blueprintjs/popover2';
 import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
 
 dayjs.extend(relativeTime);
 
+/*
+ * Detects navigator locale 24h time preference
+ * It works by checking whether hour output contains AM ('1 AM' or '01 h')
+ * based on the user's preferred language
+ */
+const isBrowserLocale24h = () =>
+  !new Intl.DateTimeFormat(navigator.language, { hour: 'numeric' })
+    .format(0)
+    .match(/AM/);
+
 function RelativeTime({ date }: { date: Date }) {
+  const is24h = isBrowserLocale24h();
+  const fmt24h = 'HH:mm';
+  const fmt12h = 'h:mm A';
+
+  const timeFormat = is24h ? fmt24h : fmt12h;
+
   return (
     <Tooltip2
-      content={dayjs(date).format('MMMM D, YYYY hh:mm A')}
+      content={dayjs(date).format(`MMMM D, YYYY ${timeFormat}`)}
       placement="bottom"
     >
-      <span>{dayjs(date).format('hh:mm A')}</span>
+      <span>{dayjs(date).format(timeFormat)}</span>
     </Tooltip2>
   );
 }
